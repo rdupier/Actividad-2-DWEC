58ba11707326cfe361c541d2a0d1a6f713bda750
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-26 17:01:48 +0100

    feat: Add fetch to ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { IProduct } from '../interfaces/iproduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private arrProducts: IProduct[] = [];
+
+  constructor() {
+    fetch("https://jsonblob.com/api/1332321945622077440")
+    .then(response => response.json())
+    .then(products => {
+      products.forEach((element: any) => {
+        let product = element as IProduct;
+        this.arrProducts.push(product);
+      });
+    });
+  }
+  
 }
