424929b2115390f34a68cd2152ac95e7121c7c03
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-26 18:59:15 +0100

    feat: Add ProductFilterComponent to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./components/product-list/product-list.component";
 import { ProductFormComponent } from "./components/product-form/product-form.component";
+import { ProductFilterComponent } from "./components/product-filter/product-filter.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ProductListComponent, ProductFormComponent],
+  imports: [ProductListComponent, ProductFormComponent, ProductFilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
