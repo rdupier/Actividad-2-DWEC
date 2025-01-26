92db00e1ac9731a3b8f2017cf790a09807096b2a
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-26 17:30:20 +0100

    feat: Upgrade ProductCardComponent

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IProduct } from '../../interfaces/iproduct';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
+
+  @Input() myProduct!: IProduct;
+  @Output() onDelete: EventEmitter<IProduct> = new EventEmitter<IProduct>();
+
+  deleteProduct(): void {
+    this.onDelete.emit(this.myProduct);
+  }
 
 }
