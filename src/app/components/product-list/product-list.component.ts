15e60d0a0a5960c6fa44fb34f85b61586de8c33f
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-26 17:20:27 +0100

    feat: Upgrade ProductListComponent

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductCardComponent } from "../product-card/product-card.component";
+import { ProductService } from '../../services/product.service';
+import { IProduct } from '../../interfaces/iproduct';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  products: IProduct[] = [];
+
+  private productService = inject(ProductService);
+  modelForm: any;
+
+  constructor() {
+    this.products = [];
+  }
+
+  ngOnInit(): void{
+    this.products = this.productService.getAllProducts();
+  }
 
 }
