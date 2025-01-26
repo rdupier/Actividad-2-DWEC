babab68d6449128cac7c2c2c590ffaf34e78ebe9
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-26 17:45:11 +0100

    feat: Add ProductFormComponent to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./components/product-list/product-list.component";
+import { ProductFormComponent } from "./components/product-form/product-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ProductListComponent],
+  imports: [ProductListComponent, ProductFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
