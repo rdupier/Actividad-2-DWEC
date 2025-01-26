a236453d43b5e7b7966d54c964cfebda6c64df1f
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-26 17:38:55 +0100

    feat: Upgrade ProductFormComponent

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../../interfaces/iproduct';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
 export class ProductFormComponent {
+
+  modelForm!: FormGroup<any>
+
+  constructor(private productService: ProductService) {
+    this.modelForm = new FormGroup ({
+    //_id: new FormControl(null,[Validators.required]),
+    name: new FormControl(null,[Validators.required, Validators.minLength(3)]),
+    description: new FormControl(null,[Validators.required, Validators.minLength(10)]),
+    price: new FormControl(null,[Validators.required, Validators.min(1)]),
+    category: new FormControl(null,[Validators.required]),
+    image: new FormControl(null,[Validators.required]),
+    //active: new FormControl(null,[Validators.required]),
+    });
+  }
+
+  getDataForm() {
+    let product: IProduct = this.modelForm.value as IProduct;
+    this.productService.addProduct(product); 
+    this.modelForm.reset();
+  }
+
+  checkControl(formControlName: string, validador: string): boolean | undefined{
+    return this.modelForm.get(formControlName)?.hasError(validador) && this.modelForm.get(formControlName)?.touched 
+  }
 
 }
