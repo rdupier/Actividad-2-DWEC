aecac25db19b8a738ed279fe0b7116aabe14d06b
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-01-27 18:22:04 +0100

    fix: Fix ProductFilterComponent

--- src/app/components/product-filter/product-filter.component.ts
+++ src/app/components/product-filter/product-filter.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-filter.component.html',
   styleUrl: './product-filter.component.css'
 })
 export class ProductFilterComponent {
+
+  @Output() onFilterChange: EventEmitter<string> = new EventEmitter<string>();
+
+  // Componente en mantenimiento: Tras realizar varios intentos con el código en el ProductFilterComponent, sigo sin conseguir que me funcione esta parte de la aplicación. 
+  // Deseando ver la corrección de la actividad para aprender más.
+  getDataForm(miFormulario: NgForm) {
+  }
 
 }
